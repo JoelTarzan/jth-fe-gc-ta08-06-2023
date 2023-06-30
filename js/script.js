0cef53f035e0e882b9b8f6d8d3b6cd60982ef1d1
Joel Tarzán <joelth368@gmail.com>
2023-06-30 12:54:31 +0200

    Puede operar

--- js/script.js
+++ js/script.js
@@ -1,31 +1,69 @@
 // Elementos del DOM
-let calcResultDiv = document.getElementById('calc-result-div');
-let btn0 = document.getElementById('btn-0');
-let btn1 = document.getElementById('btn-1');
-let btn2 = document.getElementById('btn-2');
-let btn3 = document.getElementById('btn-3');
-let btn4 = document.getElementById('btn-4');
-let btn5 = document.getElementById('btn-5');
-let btn6 = document.getElementById('btn-6');
-let btn7 = document.getElementById('btn-7');
-let btn8 = document.getElementById('btn-8');
-let btn9 = document.getElementById('btn-9');
-let btnPlus = document.getElementById('btn-+');
-let btnMinus = document.getElementById('btn--');
-let btnMultiply = document.getElementById('btn-*');
-let btnEqual = document.getElementById('btn-=');
-let btnPoint = document.getElementById('btn-.');
-let btnClear = document.getElementById('btn-C');
+let screen = document.getElementById("calc-screen");
+
+// Variables
+let elements = [];
+let elementsPostMultiplications = [];
+let result = 0;
+let operation = 'add';
 
 // Eventos
 const appendValue = (value) => {
-    calcResultDiv.innerText += value;
+    screen.innerText += value;
 };
 
-const clearResult = () => {
-    calcResultDiv.innerText = '';
+const clearScreen = () => {
+	screen.innerText = '';
+    clearArrays();
+};
+
+const clearArrays = () => {
+    elements = [];
+    elementsPostMultiplications = [];
 }
 
-const calculateResult = () => {
-    console.log(calcResultDiv.innerText);
-}
+const calculate = () => {
+	elements = screen.innerText.split(/([+\-*])/);
+    
+    // Buscamos los operadores de multiplicacion
+    for (let i = 0; i < elements.length; i++) {
+        
+        if (elements[i] == '*') {
+
+            let numPrev = Number(elements[i-1]);
+            let numPost = Number(elements[i+1]);
+            let multiplicacion = numPrev * numPost;
+
+            elementsPostMultiplications.pop(); // Corresponde al numPrev en el array
+            elementsPostMultiplications.push(multiplicacion); // Metemos el resultado
+            i += 1; // Sumamos el indice para que pase del numPost
+
+        } else {
+            elementsPostMultiplications.push(elements[i]);
+        }
+    }
+
+    for (let i = 0; i < elementsPostMultiplications.length; i++) {
+        
+        if (elementsPostMultiplications[i] == '+') {
+            operation = 'add';
+
+        } else if (elementsPostMultiplications[i] == '-') {
+            operation = 'subtract';
+
+        } else {
+
+            if (operation == 'add') {
+                result += Number(elementsPostMultiplications[i]);
+
+            } else {
+                result -= Number(elementsPostMultiplications[i]);
+            }
+        }
+    }
+
+    clearScreen();
+    appendValue(result);
+    clearArrays();
+    result = 0;
+};
